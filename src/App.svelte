0ab8cbd9ade56754e2a2df55dafd7b8173951225
Dustin Carlino <dabreegster@gmail.com>
2023-02-10 15:27:33 +0000

    Call osm2streets!

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,8 @@
   // TODO I never added this to package.json, it's a transitive dep!
   import type { Polygon } from "geojson";
   import { overpassQueryForPolygon } from "./overpass.js";
+  import init, { JsStreetNetwork } from "osm2streets-js";
+  import { onMount } from "svelte";
 
   import Layout from "./lib/Layout.svelte";
   import Map from "./lib/Map.svelte";
@@ -15,9 +17,18 @@
         kind: "osm2streets import";
         boundaryPolygon: Polygon;
         osmInput: string;
+      }
+    | {
+        kind: "done";
+        boundaryPolygon: Polygon;
+        network: JsStreetNetwork;
       };
 
   let imported: Imported = "nothing";
+
+  onMount(async () => {
+    await init();
+  });
 
   async function handlePolygon(e: CustomEvent<Polygon>) {
     let polygon = e.detail;
@@ -28,12 +39,38 @@
       let osmInput = await resp.text();
       imported = {
         kind: "osm2streets import",
+        // TODO Maybe we don't need to plumb this here
         boundaryPolygon: polygon,
         osmInput,
+      };
+
+      let network = importOSM(polygon, osmInput);
+      imported = {
+        kind: "done",
+        boundaryPolygon: polygon,
+        network,
       };
     } catch (err) {
       imported = { kind: "error", msg: err.toString() };
     }
+  }
+
+  function importOSM(
+    boundaryPolygon: Polygon,
+    osmXML: string
+  ): JsStreetNetwork {
+    let gj = {
+      type: "Feature",
+      geometry: boundaryPolygon,
+      properties: {},
+    };
+    return new JsStreetNetwork(osmXML, JSON.stringify(gj), {
+      debug_each_step: false,
+      dual_carriageway_experiment: false,
+      cycletrack_snapping_experiment: false,
+      inferred_sidewalks: false,
+      osm2lanes: false,
+    });
   }
 </script>
 
@@ -49,6 +86,8 @@
       <p>Error: {imported.msg}</p>
     {:else if imported.kind === "osm2streets import"}
       <p>got XML back! length {imported.osmInput.length}</p>
+    {:else if imported.kind === "done"}
+      <p>Got the network! {imported.network.toGeojsonPlain()}</p>
     {/if}
   </div>
   <div slot="main">
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import wasmPack from "vite-plugin-wasm-pack";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), wasmPack([], ["osm2streets-js"])],
 })
