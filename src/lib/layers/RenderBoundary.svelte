22479dae88c6626b1a0004c5b7a7d0f24be6912d
Dustin Carlino <dabreegster@gmail.com>
2023-02-13 11:10:19 +0000

    Zoom to show the imported area

--- src/lib/layers/RenderBoundary.svelte
+++ src/lib/layers/RenderBoundary.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+  import { getContext } from "svelte";
   import Layer from "../Layer.svelte";
   import type { Feature } from "geojson";
+  import bbox from "@turf/bbox";
 
   export let gj: Feature;
 
@@ -11,6 +13,11 @@
       "fill-opacity": 0.5,
     },
   };
+
+  // Initially zoom to fit the imported boundary
+  const { getMap } = getContext("map");
+  let map = getMap();
+  map.fitBounds(bbox(gj), { animate: false, padding: 200 });
 </script>
 
 <Layer source="boundary" {gj} {layerStyle} />
