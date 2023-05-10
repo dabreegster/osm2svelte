78f718e07a4b704b0f0ea25b970fc5de25994593
Dustin Carlino <dabreegster@gmail.com>
2023-05-10 13:33:54 +0100

    Render roads in route mode, scraping relevant input details

--- src/lib/modes/RouteProfileMode.svelte
+++ src/lib/modes/RouteProfileMode.svelte
@@ -16,27 +16,72 @@
     source,
     type: "fill",
     paint: {
-      "fill-color": "red",
+      "fill-color": [
+        "interpolate",
+        ["linear"],
+        ["get", "weight"],
+        0,
+        "blue",
+        0.1,
+        "royalblue",
+        0.3,
+        "cyan",
+        0.5,
+        "lime",
+        0.7,
+        "yellow",
+        1,
+        "red",
+      ],
     },
   });
   setupRoadWeights();
 
+  interface Road {
+    highway_type: string;
+    osm_id?: number;
+    // May be empty if there's not exactly 1 OSM way corresponding to the road
+    osm_tags: { [name: string]: string };
+    // TODO Can we generate TS types for these?
+    lane_specs_ltr: any[];
+  }
+
   function setupRoadWeights() {
     let gj = emptyGeojson();
 
+    let polygons = JSON.parse($streetNetwork.toGeojsonPlain());
     let network = JSON.parse($streetNetwork.toJson());
-    for (let [_, road] of network.roads) {
-      console.log(road.highway_type);
-      console.log(JSON.stringify(road.lane_specs_ltr));
+    for (let [id, road] of network.roads) {
+      let properties = {
+        highway_type: road.highway_type,
+        lane_specs_ltr: road.lane_specs_ltr,
+        osm_id: undefined,
+        osm_tags: {},
+        weight: 0.0,
+      };
       if (road.osm_ids.length == 1) {
-        let tags = JSON.parse(
-          $streetNetwork.getOsmTagsForWay(BigInt(road.osm_ids[0]))
+        properties.osm_id = road.osm_ids[0];
+        properties.osm_tags = JSON.parse(
+          $streetNetwork.getOsmTagsForWay(BigInt(properties.osm_id))
         );
-        console.log(JSON.stringify(tags));
       }
+      properties.weight = calculateWeight(properties);
+      gj.features.push({
+        type: "Feature",
+        properties,
+        geometry: polygons.features.find((f) => id == f.properties.id)!
+          .geometry,
+      });
     }
 
     ($map.getSource(source) as GeoJSONSource).setData(gj);
+  }
+
+  function calculateWeight(road: Road): number {
+    if (road.highway_type == "primary") {
+      return 0.8;
+    }
+    return 0.3;
   }
 
   onDestroy(() => {
