ee8fa90ce174630332e9969c6dda46815e24fc0e
Dustin Carlino <dabreegster@gmail.com>
2023-05-11 16:25:40 +0100

    Why is --base not working?

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 import wasmPack from "vite-plugin-wasm-pack";
 
 export default defineConfig({
+  base: "/osm2svelte/",
   plugins: [svelte(), wasmPack([], ["osm2streets-js"])],
 })
