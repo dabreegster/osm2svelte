0f1022f1a523d6f2974071bf6f909aed3e95142e
Dustin Carlino <dabreegster@gmail.com>
2023-06-13 15:11:00 -0700

    Start moving import code around, in prep for sharing it with a second page

---
+++ src/import.ts
@@ -0,0 +1,152 @@
+import { JsStreetNetwork } from "osm2streets-js";
+import { v4 as uuidv4 } from "uuid";
+// TODO I never added this to package.json, it's a transitive dep!
+import type { Imported, LayerSpec, Settings } from "./lib/types";
+import type { Feature, Polygon } from "geojson";
+import RenderIntersectionPolygons from "./lib/layers/RenderIntersectionPolygons.svelte";
+import RenderIntersectionMarkings from "./lib/layers/RenderIntersectionMarkings.svelte";
+import RenderLanePolygons from "./lib/layers/RenderLanePolygons.svelte";
+import RenderLaneMarkings from "./lib/layers/RenderLaneMarkings.svelte";
+import RenderBoundary from "./lib/layers/RenderBoundary.svelte";
+import DynamicMovementArrows from "./lib/layers/DynamicMovementArrows.svelte";
+import DynamicConnectedRoads from "./lib/layers/DynamicConnectedRoads.svelte";
+import DynamicRoadOrdering from "./lib/layers/DynamicRoadOrdering.svelte";
+import { emptyGeojson } from "./style";
+
+export function allLayers(
+  network: JsStreetNetwork,
+  boundaryGJ: Feature<Polygon>
+): LayerSpec[] {
+  return [
+    {
+      uuid: uuidv4(),
+      label: "Boundary",
+      show: true,
+      content: RenderBoundary,
+      gj: boundaryGJ,
+    },
+    {
+      uuid: uuidv4(),
+      label: "Intersection polygons",
+      show: true,
+      content: RenderIntersectionPolygons,
+      gj: JSON.parse(network.toGeojsonPlain()),
+    },
+    {
+      uuid: uuidv4(),
+      label: "Intersection markings",
+      show: true,
+      content: RenderIntersectionMarkings,
+      gj: JSON.parse(network.toIntersectionMarkingsGeojson()),
+    },
+    {
+      uuid: uuidv4(),
+      label: "Lane polygons",
+      show: true,
+      content: RenderLanePolygons,
+      gj: JSON.parse(network.toLanePolygonsGeojson()),
+    },
+    {
+      uuid: uuidv4(),
+      label: "Lane markings",
+      show: true,
+      content: RenderLaneMarkings,
+      gj: JSON.parse(network.toLaneMarkingsGeojson()),
+    },
+    {
+      uuid: uuidv4(),
+      label: "Movement arrows",
+      show: true,
+      content: DynamicMovementArrows,
+      gj: emptyGeojson(),
+    },
+    {
+      uuid: uuidv4(),
+      label: "Roads connected to intersection",
+      show: true,
+      content: DynamicConnectedRoads,
+      gj: emptyGeojson(),
+    },
+    {
+      uuid: uuidv4(),
+      label: "Clockwise ordering of roads",
+      show: false,
+      content: DynamicRoadOrdering,
+      gj: emptyGeojson(),
+    },
+  ];
+
+  // TODO Some bizarre syntax error
+  /*debugLayers = debugSteps.map((step) => {
+          uuid: uuidv4(),
+          label: `${step.getLabel()}: Intersection polygons`,
+          show: true,
+          content: RenderIntersectionPolygons,
+          gj: JSON.parse(step.getNetwork().toGeojsonPlain()),
+      });
+      debugLayers = [];
+      for (let step of network.getDebugSteps()) {
+        debugLayers.push({
+          uuid: uuidv4(),
+          label: `${step.getLabel()}: Intersection polygons`,
+          show: true,
+          content: RenderIntersectionPolygons,
+          gj: JSON.parse(step.getNetwork().toGeojsonPlain()),
+        });
+      }*/
+}
+
+export async function importPolygon(
+  e: CustomEvent<Polygon>,
+  imported: Imported,
+  settings: Settings
+) {
+  let polygon = e.detail;
+  imported = { kind: "LoadingOverpass", polygon };
+
+  try {
+    let resp = await fetch(overpassQueryForPolygon(polygon));
+    let osmInput = await resp.text();
+    imported = {
+      kind: "osm2streets import",
+      // TODO Maybe we don't need to plumb this here
+      boundaryPolygon: polygon,
+      osmInput,
+    };
+
+    let [network, boundaryGJ] = importOSM(polygon, osmInput, settings);
+    imported = {
+      kind: "done",
+      boundaryGJ,
+      network,
+      osmInput,
+    };
+  } catch (err) {
+    imported = { kind: "error", msg: err.toString() };
+  }
+}
+
+function importOSM(
+  boundaryPolygon: Polygon,
+  osmXML: string,
+  settings: Settings
+): [JsStreetNetwork, Feature<Polygon>] {
+  let gj: Feature<Polygon> = {
+    type: "Feature",
+    geometry: boundaryPolygon,
+    properties: {},
+  };
+  return [new JsStreetNetwork(osmXML, JSON.stringify(gj), settings), gj];
+}
+
+function overpassQueryForPolygon(polygon: Polygon): string {
+  // Construct a query to extract all XML data in the polygon clip. See
+  // https://wiki.openstreetmap.org/wiki/Overpass_API/Overpass_QL
+  let filter = 'poly:"';
+  for (let [lng, lat] of polygon.coordinates[0]) {
+    filter += `${lat} ${lng} `;
+  }
+  filter = filter.slice(0, -1) + '"';
+  let query = `(nwr(${filter}); node(w)->.x; <;); out meta;`;
+  return `https://overpass-api.de/api/interpreter?data=${query}`;
+}
--- src/pages/StreetExplorer.svelte
+++ src/pages/StreetExplorer.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
   // TODO I never added this to package.json, it's a transitive dep!
   import type { Feature, Polygon } from "geojson";
-  import { overpassQueryForPolygon } from "../overpass";
   import init, { JsStreetNetwork } from "osm2streets-js";
   import { onMount } from "svelte";
   import { network, boundaryGJ } from "../store";
-  import { emptyGeojson } from "../style";
   import type { Imported, LayerSpec, Settings } from "../lib/types";
-  import { v4 as uuidv4 } from "uuid";
 
+  import { allLayers, importPolygon } from "../import";
   import Layout from "../lib/Layout.svelte";
   import Map from "../lib/Map.svelte";
   import SelectImportArea from "../lib/SelectImportArea.svelte";
@@ -19,15 +17,6 @@
   import SequentialLayerGroup from "../lib/SequentialLayerGroup.svelte";
   import ImportControls from "../lib/ImportControls.svelte";
   import BuiltinImporter from "../lib/BuiltinImporter.svelte";
-
-  import RenderIntersectionPolygons from "../lib/layers/RenderIntersectionPolygons.svelte";
-  import RenderIntersectionMarkings from "../lib/layers/RenderIntersectionMarkings.svelte";
-  import RenderLanePolygons from "../lib/layers/RenderLanePolygons.svelte";
-  import RenderLaneMarkings from "../lib/layers/RenderLaneMarkings.svelte";
-  import RenderBoundary from "../lib/layers/RenderBoundary.svelte";
-  import DynamicMovementArrows from "../lib/layers/DynamicMovementArrows.svelte";
-  import DynamicConnectedRoads from "../lib/layers/DynamicConnectedRoads.svelte";
-  import DynamicRoadOrdering from "../lib/layers/DynamicRoadOrdering.svelte";
 
   import InfoMode from "../lib/modes/InfoMode.svelte";
   import ThickenRoadsMode from "../lib/modes/ThickenRoadsMode.svelte";
@@ -50,123 +39,8 @@
       network.set(imported.network);
       boundaryGJ.set(imported.boundaryGJ);
 
-      // Set layers
-      layers = [
-        {
-          uuid: uuidv4(),
-          label: "Boundary",
-          show: true,
-          content: RenderBoundary,
-          gj: $boundaryGJ,
-        },
-        {
-          uuid: uuidv4(),
-          label: "Intersection polygons",
-          show: true,
-          content: RenderIntersectionPolygons,
-          gj: JSON.parse($network!.toGeojsonPlain()),
-        },
-        {
-          uuid: uuidv4(),
-          label: "Intersection markings",
-          show: true,
-          content: RenderIntersectionMarkings,
-          gj: JSON.parse($network!.toIntersectionMarkingsGeojson()),
-        },
-        {
-          uuid: uuidv4(),
-          label: "Lane polygons",
-          show: true,
-          content: RenderLanePolygons,
-          gj: JSON.parse($network!.toLanePolygonsGeojson()),
-        },
-        {
-          uuid: uuidv4(),
-          label: "Lane markings",
-          show: true,
-          content: RenderLaneMarkings,
-          gj: JSON.parse($network!.toLaneMarkingsGeojson()),
-        },
-        {
-          uuid: uuidv4(),
-          label: "Movement arrows",
-          show: true,
-          content: DynamicMovementArrows,
-          gj: emptyGeojson(),
-        },
-        {
-          uuid: uuidv4(),
-          label: "Roads connected to intersection",
-          show: true,
-          content: DynamicConnectedRoads,
-          gj: emptyGeojson(),
-        },
-        {
-          uuid: uuidv4(),
-          label: "Clockwise ordering of roads",
-          show: false,
-          content: DynamicRoadOrdering,
-          gj: emptyGeojson(),
-        },
-      ];
-
-      // TODO Some bizarre syntax error
-      /*debugLayers = debugSteps.map((step) => {
-          uuid: uuidv4(),
-          label: `${step.getLabel()}: Intersection polygons`,
-          show: true,
-          content: RenderIntersectionPolygons,
-          gj: JSON.parse(step.getNetwork().toGeojsonPlain()),
-      });
-      debugLayers = [];
-      for (let step of $network!.getDebugSteps()) {
-        debugLayers.push({
-          uuid: uuidv4(),
-          label: `${step.getLabel()}: Intersection polygons`,
-          show: true,
-          content: RenderIntersectionPolygons,
-          gj: JSON.parse(step.getNetwork().toGeojsonPlain()),
-        });
-      }*/
+      layers = allLayers(imported.network, imported.boundaryGJ);
     }
-  }
-
-  async function handlePolygon(e: CustomEvent<Polygon>) {
-    let polygon = e.detail;
-    imported = { kind: "LoadingOverpass", polygon };
-
-    try {
-      let resp = await fetch(overpassQueryForPolygon(polygon));
-      let osmInput = await resp.text();
-      imported = {
-        kind: "osm2streets import",
-        // TODO Maybe we don't need to plumb this here
-        boundaryPolygon: polygon,
-        osmInput,
-      };
-
-      let [network, boundaryGJ] = importOSM(polygon, osmInput);
-      imported = {
-        kind: "done",
-        boundaryGJ,
-        network,
-        osmInput,
-      };
-    } catch (err) {
-      imported = { kind: "error", msg: err.toString() };
-    }
-  }
-
-  function importOSM(
-    boundaryPolygon: Polygon,
-    osmXML: string
-  ): [JsStreetNetwork, Feature<Polygon>] {
-    let gj: Feature<Polygon> = {
-      type: "Feature",
-      geometry: boundaryPolygon,
-      properties: {},
-    };
-    return [new JsStreetNetwork(osmXML, JSON.stringify(gj), settings), gj];
   }
 </script>
 
@@ -189,7 +63,9 @@
   </div>
   <div slot="main">
     <Map>
-      <SelectImportArea on:polygon={handlePolygon} />
+      <SelectImportArea
+        on:polygon={(e) => importPolygon(e, imported, settings)}
+      />
       <LayerGroup {layers}>
         <SequentialLayerGroup layers={debugLayers} />
         <VectorTileControls />
