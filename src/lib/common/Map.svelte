382f2eaa073413f52c9244b5e63f9c7b71be64f8
Dustin Carlino <dabreegster@gmail.com>
2023-06-30 13:41:43 +0100

    Initially switch to svelte-maplibre. Just swap out the Map component

--- src/lib/common/Map.svelte
+++ src/lib/common/Map.svelte
@@ -1,52 +1,15 @@
 <script lang="ts">
-  import { Map, NavigationControl, ScaleControl } from "maplibre-gl";
-  import { onDestroy, onMount } from "svelte";
-  import "maplibre-gl/dist/maplibre-gl.css";
-  import { map as mapStore } from "../../store";
-
-  let map: Map;
-  let mapContainer: HTMLDivElement;
-  let loaded = false;
-
-  onMount(() => {
-    map = new Map({
-      container: mapContainer,
-      style:
-        "https://api.maptiler.com/maps/streets/style.json?key=MZEJTanw3WpxRvt7qDfo",
-      hash: true,
-    });
-    map.addControl(new ScaleControl({}));
-    map.addControl(new NavigationControl({}), "bottom-right");
-
-    map.on("load", () => {
-      loaded = true;
-      // Debugging
-      //window.map = map;
-      mapStore.set(map);
-    });
-
-    const resizeObserver = new ResizeObserver(() => {
-      map.resize();
-    });
-    resizeObserver.observe(mapContainer);
-  });
-
-  onDestroy(() => {
-    map.remove();
-  });
+  import { MapLibre, NavigationControl, ScaleControl } from "svelte-maplibre";
+  import { map } from "../../store";
 </script>
 
-<div class="map" bind:this={mapContainer}>
-  {#if loaded}
+<div style="position:relative; width: 100%; height: 100vh;">
+  <MapLibre
+    style="https://api.maptiler.com/maps/streets/style.json?key=MZEJTanw3WpxRvt7qDfo"
+    bind:map={$map}
+  >
+    <NavigationControl position="bottom-right" />
+    <ScaleControl />
     <slot />
-  {/if}
+  </MapLibre>
 </div>
-
-<style>
-  .map {
-    position: relative;
-    flex-grow: 1;
-    /* TODO: Hack, can't figure out why height broken */
-    min-height: 100vh;
-  }
-</style>
