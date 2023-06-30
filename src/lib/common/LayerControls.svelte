442b2ee2e0d89a2bcb953e034242fd764fd7cc52
Dustin Carlino <dabreegster@gmail.com>
2023-06-30 13:59:24 +0100

    Use svelte-maplibre for the boundary layer
    ... But the source doesn't seem idempotent. Are we doing something
    horrible and creating the component many times currently?

---
+++ src/lib/common/LayerControls.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { downloadGeneratedFile } from "../../utils";
+
+  export let gj: GeoJSON;
+  export let downloadable;
+
+  function download() {
+    // TODO Plumb down a name/label for this layer?
+    downloadGeneratedFile("layer.geojson", JSON.stringify(gj));
+  }
+</script>
+
+{#if downloadable}
+  <button type="button" on:click={download}>Download</button>
+{/if}
--- src/lib/layers/RenderBoundary.svelte
+++ src/lib/layers/RenderBoundary.svelte
@@ -2,23 +2,23 @@
   import type { GeoJSON } from "geojson";
   import { map } from "../../store";
   import { bbox } from "../../style";
-  import Layer from "../common/Layer.svelte";
+  import { GeoJSON, FillLayer, LineLayer } from "svelte-maplibre";
+  import LayerControls from "../common/LayerControls.svelte";
 
   // TODO Is there a way to plumb a bunch of generic Layer props like this?
   export let gj: GeoJSON;
   export let show: boolean;
   export let downloadable: boolean;
 
-  let layerStyle = {
-    type: "line",
-    paint: {
-      "line-color": "blue",
-      "line-width": 4,
-    },
-  };
-
   // Initially zoom to fit the imported boundary
   $map.fitBounds(bbox(gj), { animate: false, padding: 10 });
 </script>
 
-<Layer source="boundary" {gj} {layerStyle} bind:show {downloadable} />
+<GeoJSON id="boundary" data={gj}>
+  {#if show}
+    <FillLayer paint={{"fill-color": "blue", "fill-opacity": 0.3}} />
+    <LineLayer paint={{"line-color": "blue", "line-width": 4}} />
+  {/if}
+</GeoJSON>
+
+<LayerControls {gj} {downloadable} />
